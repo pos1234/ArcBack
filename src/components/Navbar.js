eada193e7a3484f8865a9b711c32beda37a8be38
paulos <paulosegebeyehu5@gmail.com>
2023-03-07 11:29:29 -0800

    jhgfg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import {NavLink} from 'react-router-dom'
+
 const Navbar = ()=>{
     return(
         <div className="navigation">
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -9,8 +10,8 @@ import News from './components/News.js'
 import Clients from './components/Clients.js'
 import Contact from './components/Contact.js'
 import './index.css'
-/* import './assets/fonts/css/all.min.css' 
- */
+import './assets/fonts/css/all.min.css' 
+ 
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
 /* ReactDOM.render(<App />,document.getElementById('root'))
